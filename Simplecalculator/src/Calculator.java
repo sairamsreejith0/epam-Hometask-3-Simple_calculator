5947f8c970be7c69d203c46b109c04438d88cc05
sairamsreejith0 <30547601+sairamsreejith0@users.noreply.github.com>
2020-02-06 21:52:05 +0530

    Add files via upload

---
+++ Simplecalculator/src/Calculator.java
@@ -0,0 +1,21 @@
+
+public class Calculator {
+	 public float sum(float a,float b)
+	 {
+		 return a+b;
+	 }
+	 public float difference(float a,float b)
+	 { 
+         return a-b;
+	 }
+	public float division(float a,float b)
+	 {
+		 return a/b;
+	 }
+	 
+	 public float multiplication(float a,float b)
+	 {
+		 return a*b;
+	 }
+	
+}
